fc38a7804692cf2acc65d923b21b877b342522a1
benmcgarvey <ben@sidekicker.com.au>
2021-07-05 21:41:24 +1000

    initial commit

---
+++ src/muthur.ts
@@ -0,0 +1,15 @@
+import { App } from '@slack/bolt';
+require('dotenv').config();
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  // Start your app
+  await app.start(3000);
+
+  console.log('muthur is listening');
+})();
