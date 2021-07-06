30cf452f3529a862384a1e719e663f05b25a344c
benmcgarvey <ben@sidekicker.com.au>
2021-07-06 17:56:38 +1000

    ignores dist

--- dist/assert.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function assert(condition, errorMessage) {
-    if (!condition) {
-        throw new Error(errorMessage);
-    }
-}
-exports.default = assert;
